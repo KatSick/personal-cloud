8ddc971685e0b7126465b764c4edb11237f3bc74
Ostap Chervak <ostap.chervak@grammarly.com>
2022-05-26 11:33:33 +0300

    fix(docker): do not expose socket to the public

--- setup-alpine.sh
+++ setup-alpine.sh
@@ -5,4 +5,3 @@ addgroup root docker
 rc-update add docker boot
 service docker start
 apk add docker-compose
-echo '{"hosts": ["unix:///var/run/docker.sock", "tcp://0.0.0.0:2375"]}' > /etc/docker/daemon.json # expose docker port outside. since we use ssh connect, it is OK
